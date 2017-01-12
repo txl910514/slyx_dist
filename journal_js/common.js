082908d2ba3ac1993692b979bdb26899858bc169
tangxlBurnish <tangxl@burnish.cn>
2017-01-12 21:31:22 +0800

    适配页面

--- journal_js/common.js
+++ journal_js/common.js
@@ -1,1 +1,290 @@
-GVR.JSON.area_json={db:["东北","黑龙江","吉林","辽宁"],hb:["华北","北京","河北","内蒙古","天津"],hd:["华东","安徽","江苏","山东","上海","浙江"],hn:["华南","广东","广西","海南"],hz:["华中","福建","湖北","湖南","江西"],xb:["西北","甘肃","河南","宁夏","青海","山西","陕西","新疆"],xn:["西南","贵州","四川","西藏","云南","重庆"]},GVR.JSON.provinceArray=["黑龙江","吉林","辽宁","北京","河北","内蒙古","天津","安徽","江苏","山东","上海","浙江","广东","广西","海南","福建","湖北","湖南","江西","甘肃","河南","宁夏","青海","山西","陕西","新疆","贵州","四川","西藏","云南","重庆","台湾","香港","澳门"],GVR.JSON.provinceJson=["heilongjiang","jilin","liaoning","beijing","hebei","neimenggu","tianjin","anhui","jiangsu","shandong","shanghai","zhejiang","guangdong","guangxi","hainan","fujian","hubei","hunan","jiangxi","gansu","henan","ningxia","qinghai","shanxi","shanxi1","xinjiang","guizhou","sichuan","xizang","yunnan","chongqing","tw","xianggang","aomen"];var COMMON_FUNC={ready_init:function(){var e=this;e.get_time()},get_time:function(){function e(){var a,i=new Date,n=i.getHours(),t=i.getMinutes(),r=i.getFullYear(),o=i.getMonth()+1,l=i.getDate();n=n<10?"0"+n:n,t=t<10?"0"+t:t,o=o<10?"0"+o:o,l=l<10?"0"+l:l,a=r+"年"+o+"月"+l+"日",$("#js-hour-text").text(n),$("#js-min-text").text(t),$("#js-year-text").text(a),setTimeout(e,1e3)}e()},ajax_get:function(e,a,i,n,t,r){var o=this,l=Array.prototype.slice.call(arguments),s=l.slice(-1)[0];if(t=null,call_back=null,_.isFunction(s)){var r=l.pop(),c=l.slice(-1)[0];_.isFunction(c)&&(t=l.pop()),e=l[0],a=l[1],i=l[2],n=l[3]}if(!e.hasClass("disabled")){e.addClass("disabled");new Date;a=a||{};var i=i||e.attr("url")||".";return n=n||e.attr("jsonp-callback"),$.ajax({type:"GET",url:i,data:a,dataType:"JSONP",jsonp:"callback",jsonpCallback:n,success:function(a){a.msg&&console.log(a.msg),e.removeClass("disabled"),_.isFunction(r)&&r(a),1===a.reload?o._reload():2===a.reload?o.reload():3===a.reload?o._reload(0):a.redirect&&o.redirect(a.redirect)},error:function(a,i,n){"error"===i?404===a.status?console.info("无效的数据"):500===a.status?console.info("服务器异常，请联系管理员"):403===a.status?(console.info("登录已过期或没有访问权限"),o._reload()):console.info("网络异常，请稍后再试"):i&&console.log(i),_.isFunction(t)&&t(),e.removeClass("disabled")}})}return!1},ajax_post:function(e,a,i,n,t,r){var o=this,l=Array.prototype.slice.call(arguments),s=l.slice(-1)[0];if(t=null,call_back=null,_.isFunction(s)){var r=l.pop(),c=l.slice(-1)[0];_.isFunction(c)&&(t=l.pop()),e=l[0],a=l[1],i=l[2],n=l[3]}return o._ajax_post(e,a,i,n,{},t,r)},_ajax_post:function(e,a,i,n,t,r,o){var l=this,s=Array.prototype.slice.call(arguments),c=s.slice(-1)[0];if(r=null,call_back=null,_.isFunction(c)){var o=s.pop(),d=s.slice(-1)[0];_.isFunction(d)&&(r=s.pop()),e=s[0],a=s[1],i=s[2],n=s[3],t=s[4]}if(e.hasClass("disabled"))l.full_loading("hide");else{var u=i||e.closest("form");console.log(u);var n=n||u.attr("url")||u.attr("action"),g=!1;if(u.find(".necessary[disabled!='disabled']").each(function(){if(!$(this).val()){var e=$(this).attr("placeholder")||$(this).prev("label").text()||$(this).prev(".pl").text()||$(this).closest(".control-group").find("label").text();e=e.replace(/[：:]/g,""),alertify.error(e+"不能为空"),g=!0}}),!g){e.addClass("disabled");var a=a||u.serialize(),h={type:"POST",url:n,data:a,headers:{"X-CSRFToken":$.cookie("csrftoken")},success:function(a){a.msg&&alertify[a.stat](a.msg),e.removeClass("disabled"),_.isFunction(o)&&o(a),1===a.reload?l._reload():2===a.reload?l.reload():3===a.reload?l._reload(0):a.redirect?l.redirect(a.redirect):a.close_modal&&$(".modal").modal("hide"),l.full_loading("hide")},error:function(a,i,n){"error"===i?404===a.status?alertify.error("无效的数据"):500===a.status?alertify.error("服务器异常，请联系管理员"):403===a.status?(alertify.error("登录已过期或没有访问权限"),l._reload()):alertify.error("网络异常，请稍后再试"):i&&alertify.error(i),_.isFunction(r)&&r(),e.removeClass("disabled"),l.full_loading("hide")}};return _.isObject(t)&&_.extend(h,t),$.ajax(h)}}return!1}};$(function(){COMMON_FUNC.ready_init()}).on("click",".img-rotate",function(){COMMON_FUNC.area_map($(this))});
+/**
+ * Created by tangxl on 16-12-6.
+ */
+GVR.JSON.area_json = {
+  db: ['东北','黑龙江','吉林', '辽宁'],
+  hb: ['华北','北京', '河北', '内蒙古', '天津'],
+  hd: ['华东','安徽', '江苏', '山东', '上海','浙江'],
+  hn: ['华南', '广东', '广西', '海南'],
+  hz: ['华中', '福建', '湖北', '湖南', '江西'],
+  xb: ['西北','甘肃', '河南', '宁夏', '青海', '山西', '陕西', '新疆'],
+  xn: ['西南','贵州', '四川', '西藏', '云南','重庆']
+};
+GVR.JSON.provinceArray = ['黑龙江','吉林', '辽宁', '北京', '河北',
+  '内蒙古', '天津','安徽', '江苏', '山东', '上海','浙江','广东', '广西', '海南',
+  '福建', '湖北', '湖南', '江西', '甘肃', '河南', '宁夏', '青海', '山西', '陕西', '新疆',
+  '贵州', '四川', '西藏', '云南','重庆', '台湾', '香港', '澳门'];
+GVR.JSON.provinceJson = ['heilongjiang','jilin', 'liaoning', 'beijing', 'hebei',
+  'neimenggu', 'tianjin','anhui', 'jiangsu', 'shandong', 'shanghai','zhejiang',
+  'guangdong', 'guangxi', 'hainan', 'fujian', 'hubei', 'hunan', 'jiangxi', 'gansu',
+  'henan', 'ningxia', 'qinghai', 'shanxi', 'shanxi1', 'xinjiang', 'guizhou', 'sichuan',
+  'xizang', 'yunnan','chongqing', 'tw', 'xianggang', 'aomen'];
+var COMMON_FUNC = {
+  ready_init: function() {
+    var self = this;
+    self.get_time();
+  },
+
+  get_time: function() {
+    function getTime_func() {
+      var time_text;
+      var local_time = new Date();
+      var hour = local_time.getHours();
+      var min = local_time.getMinutes();
+      var year = local_time.getFullYear();
+      var month = local_time.getMonth()+1;
+      var date = local_time.getDate();
+      hour = hour < 10 ? '0' + hour : hour;
+      min = min < 10 ? '0' + min : min;
+      month = month < 10 ? '0' + month : month;
+      date = date < 10 ? '0' + date : date;
+      time_text = year + '年' + month + '月' + date + '日';
+      $('#js-hour-text').text(hour);
+      $('#js-min-text').text(min);
+      $('#js-year-text').text(time_text);
+      setTimeout(getTime_func, 1000);
+    }
+    getTime_func();
+  },
+
+  ajax_get: function($obj, data, url, jsonp_name, error_callback, callback){
+    /*
+     ajax get通用方法
+     参数：
+     $obj: 触发GET事件的元数对象，一般为a标签或button。
+     data (可选): 往URL上附加的额外参数。
+     url (可选): GET请求的URL路径，不传会从$obj对象上获取url属性，没有url属性时默认用当前路径.
+     error_callback (可选): 异常时的回调函数
+     callback: 成功时的回调函数
+     */
+    var self = this;
+    var args = Array.prototype.slice.call(arguments);
+    var _callback = args.slice(-1)[0];
+    error_callback = null;
+    call_back = null;
+    if(_.isFunction(_callback)){
+      var callback = args.pop();
+      var _error_callback = args.slice(-1)[0];
+      if(_.isFunction(_error_callback)){
+        error_callback =  args.pop();
+      }
+      $obj = args[0];
+      data = args[1];
+      url = args[2];
+      jsonp_name = args[3];
+    }
+    if($obj.hasClass('disabled')){
+/*      self.full_loading("hide");*/
+    }
+    else{
+      $obj.addClass('disabled');
+      var my_date = new Date;
+      data = data || {};
+/*      $.extend(data, {time: my_date.getTime()});*/
+      var url= url || $obj.attr('url') || '.';
+      jsonp_name = jsonp_name || $obj.attr('jsonp-callback');
+      return $.ajax({
+        type: 'GET',
+        url: url,
+        data: data,
+        dataType:'JSONP',
+        jsonp: 'callback',
+        jsonpCallback:jsonp_name,
+        success: function(result){
+          if(result.msg){
+            console.log(result.msg);
+          }
+          $obj.removeClass('disabled');
+          if(_.isFunction(callback)){
+            callback(result);
+          }
+          if(result.reload === 1){
+            self._reload();
+          }
+          else if(result.reload === 2){
+            self.reload();
+          }
+          else if(result.reload === 3){
+            self._reload(0);
+          }
+          else if(result.redirect){
+            self.redirect(result.redirect);
+          }
+/*          self.full_loading("hide");*/
+        },
+        error: function(xhr, msg, error){
+          if(msg === "error"){
+            if(xhr.status === 404){
+              console.info("无效的数据");
+            }
+            else if(xhr.status === 500){
+              console.info("服务器异常，请联系管理员");
+            }
+            else if(xhr.status === 403){
+              console.info("登录已过期或没有访问权限");
+              self._reload();
+            }
+            else{
+              console.info("网络异常，请稍后再试");
+            }
+          }
+          else{
+            if(msg){
+              console.log(msg);
+            }
+          }
+          if(_.isFunction(error_callback)){
+            error_callback();
+          };
+          $obj.removeClass('disabled');
+/*          self.full_loading("hide");*/
+        }
+      });
+    }
+    return false;
+  },
+
+  ajax_post: function ($obj, data, form, action, error_callback, callback){
+    /*
+     ajax post通用方法
+     参数：
+     $obj: form 表单内的一个元素对象，一般为提交按钮或输入框。
+     data (可选): 要提交的内容， 不传时会以form序列化内容为准。
+     form (可选): form 表单对象，不传时会从$obj找最近的父级form元数
+     action (可选): POST请求的url路径，不传时会用form的action属性。
+     error_callback (可选): 异常时的回调函数
+     callback: 成功时的回调函数
+     */
+    var self = this;
+    var args = Array.prototype.slice.call(arguments);
+    var _callback = args.slice(-1)[0];
+    error_callback = null;
+    call_back = null;
+    if(_.isFunction(_callback)){
+      var callback = args.pop();
+      var _error_callback = args.slice(-1)[0];
+      if(_.isFunction(_error_callback)){
+        error_callback = args.pop();
+      }
+      $obj = args[0];
+      data = args[1];
+      form = args[2];
+      action = args[3];
+    }
+    return self._ajax_post($obj, data, form, action, {}, error_callback, callback)
+  },
+
+  _ajax_post: function($obj, data, form, action, ajax_options, error_callback, callback){
+    var self = this;
+    var args = Array.prototype.slice.call(arguments);
+    var _callback = args.slice(-1)[0];
+    error_callback = null;
+    call_back = null;
+    if(_.isFunction(_callback)){
+      var callback = args.pop();
+      var _error_callback = args.slice(-1)[0];
+      if(_.isFunction(_error_callback)){
+        error_callback =  args.pop();
+      }
+      $obj = args[0];
+      data = args[1];
+      form = args[2];
+      action = args[3];
+      ajax_options= args[4];
+    }
+    if($obj.hasClass('disabled')){
+      self.full_loading("hide");
+    }
+    else{
+      var $form = form || $obj.closest('form');
+      console.log($form);
+      var action = action || $form.attr("url") || $form.attr('action');
+      var empty = false;
+      $form.find(".necessary[disabled!='disabled']").each(function(){
+        if(!($(this).val())){
+          var label = $(this).attr('placeholder')
+            || $(this).prev('label').text()
+            || $(this).prev('.pl').text()
+            || $(this).closest('.control-group').find('label').text();
+          label = label.replace(/[：:]/g, "");
+          alertify.error(label + '不能为空');
+          empty = true;
+        }
+      });
+      if(!empty){
+        $obj.addClass('disabled');
+        var data = data || $form.serialize();
+        var options = {
+          type: 'POST',
+          url: action,
+          data: data,
+          headers: {"X-CSRFToken": $.cookie("csrftoken")},
+          success: function(result){
+            if(result.msg){
+              alertify[result.stat](result.msg)
+            }
+            $obj.removeClass('disabled');
+            if(_.isFunction(callback)){
+              callback(result);
+            }
+            if(result.reload === 1){
+              self._reload();
+            }
+            else if(result.reload === 2){
+              self.reload();
+            }
+            else if(result.reload === 3){
+              self._reload(0);
+            }
+            else if(result.redirect){
+              self.redirect(result.redirect);
+            }
+            else if(result.close_modal){
+              $(".modal").modal("hide");
+            }
+            self.full_loading("hide");
+          },
+          error: function(xhr, msg, error) {
+            if(msg === "error"){
+              if(xhr.status === 404){
+                alertify.error("无效的数据");
+              }
+              else if(xhr.status === 500){
+                alertify.error("服务器异常，请联系管理员");
+              }
+              else if(xhr.status === 403){
+                alertify.error("登录已过期或没有访问权限");
+                self._reload();
+              }
+              else{
+                alertify.error("网络异常，请稍后再试");
+              }
+            }
+            else{
+              if(msg){
+                alertify.error(msg);
+              }
+            }
+            if(_.isFunction(error_callback)){
+              error_callback();
+            }
+            $obj.removeClass('disabled');
+            self.full_loading("hide");
+          }
+        }
+        if(_.isObject(ajax_options)){
+          _.extend(options, ajax_options);
+        }
+        return $.ajax(options);
+      }
+    }
+    return false;
+  }
+
+};
+
+$(function() {
+  COMMON_FUNC.ready_init();
+}).on('click', '.img-rotate', function() {
+  COMMON_FUNC.area_map($(this));
+});
--- journal_js/index.js
+++ journal_js/index.js
@@ -1,1 +1,80 @@
-var index={ready_init:function(){$(".hoop-box").each(function(i,n){var t=$(n),o=t.width();t.css({height:o+"px"})}),$(".summing-head").each(function(i,n){var t=$(n),o=t.width(),e=58*o/660;t.css({height:e+"px"})});new Swiper(".swiper-container",{direction:"vertical",loop:!1})},input_focus:function(i){setTimeout(function(){var n=i.parent(),t=n.position().top,o=n.closest(".apply-form-slide");o.scrollTop(t)},500)}};$(function(){index.ready_init()}).on("click",".input-line input",function(i){index.input_focus($(this))});
+/**
+ * Created by tangxl on 16-12-6.
+ */
+var index = {
+  ready_init:function() {
+    var self = this;
+    var one_scale = 315/500;
+    var two_scale = 226/500;
+    var three_scale = 148/500;
+    var bar_width = $('.cup-bar').width();
+    var one_bottom = bar_width*one_scale;
+    var two_bottom = bar_width*two_scale;
+    var three_bottom = bar_width*three_scale;
+    var one_height = $('.award1').height();
+    var two_height = $('.award2').height();
+    var three_height = $('.award3').height();
+    $('.award1').css({
+      bottom:one_bottom +'px'
+    });
+    $('.award2').css({
+      bottom:two_bottom +'px'
+    });
+    $('.award3').css({
+      bottom:three_bottom +'px'
+    });
+    $('.technical-num1').css({
+      bottom:(one_bottom-40) +'px'
+    });
+    $('.technical-num2').css({
+      bottom:(two_bottom-40) +'px'
+    });
+    $('.technical-num3').css({
+      bottom:(three_bottom-40) +'px'
+    });
+    $('.technical-name1').css({
+      bottom:(one_bottom + one_height + 15) +'px'
+    });
+    $('.technical-name2').css({
+      bottom:(two_bottom + one_height+ 15) +'px'
+    });
+    $('.technical-name3').css({
+      bottom:(three_bottom + one_height+ 15) +'px'
+    });
+    $('.hoop-box').each(function(index, dom) {
+      var $dom = $(dom);
+      var width = $dom.width();
+      $dom.css({
+        height: width + 'px'
+      });
+    });
+    $('.summing-head').each(function(index, dom) {
+      var $dom = $(dom);
+      var width = $dom.width();
+      var height = width*58 / 660;
+      $dom.css({
+        height: height + 'px'
+      });
+    });
+    var mySwiper = new Swiper('.swiper-container', {
+      direction : 'vertical',
+      loop: false
+    });
+  },
+
+  input_focus: function($obj) {
+    setTimeout(function() {
+      var $input_parent = $obj.parent();
+      var posioton_top = $input_parent.position().top;
+      var $apply_form_slide = $input_parent.closest('.apply-form-slide');
+      $apply_form_slide.scrollTop(posioton_top);
+    }, 500);
+  }
+};
+
+$(function(){
+  index.ready_init();
+}).on('click', '.input-line input', function(e) {
+  index.input_focus($(this));
+})
+;
